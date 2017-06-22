2748872d087676cecd40b1c7df47c07d2eff775a
Alex-Kinuthia <alexkinuthia125@gmail.com>
2017-06-22 08:47:11 +0300

    new files

---
+++ app/src/main/java/com/example/alex/mymood/MainActivity.java
@@ -0,0 +1,17 @@
+package com.example.alex.mymood;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.Button;
+
+/**
+ * Created by alex on 5/25/17.
+ */
+
+public class MainActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
